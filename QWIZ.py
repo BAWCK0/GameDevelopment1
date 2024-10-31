41eb574a33aeb9fc58ec08762176f5ef545c18ea
BAWCK0 <sorton9.208@gmail.com>
2024-10-31 18:50:39 +0000

    QWIZ.py

--- QWIZ.py
+++ QWIZ.py
@@ -1,6 +1,6 @@
 import pgzrun
-HEIGHT = 600
-WIDTH = 800
+HEIGHT = 700
+WIDTH = 900
 title = "qwiz master"
 marquee_box = Rect(0,0,880,80)
 question_box = Rect(0,0,650,150)
@@ -11,7 +11,7 @@ answer_box2 = Rect(0,0,300,150)
 answer_box3 = Rect(0,0,300,150)
 answer_box4 = Rect(0,0,300,150)
 score = 0
-time_left = 10
+time_left = 100
 question_filename = "questions.txt"
 marquee_message = ""
 is_gameover = False
@@ -54,7 +54,7 @@ def move_marquee():
         marquee_box.left = WIDTH
 
 def read_question_file():
-    global question_count, questions
+    global question_count, questions #global hamster, microwaves, dinosaurs_from_mars
     q_file = open(question_filename, "r")
     for question in q_file:
         questions.append(question)
@@ -69,8 +69,8 @@ def read_next_question():
 def on_mouse_down(pos):
     index = 1
     for box in answer_boxes:
-        if box.collidepoint(pos):
-            if index is int(question[5]):
+        if box.collidepoint(pos): # Checks if cursor is on the question boxes / which on it's on
+            if index is int(question[5]): # Check if answer is correct or not
                 correct_answer()
             else:
                 gameover()
@@ -83,14 +83,14 @@ def correct_answer():
     score += 1
     if questions:
         question = read_next_question()
-        time_left = 10
+        time_left = 100
     else:
         gameover()
 
 def gameover():
     global question, time_left, is_gameover
     message = f"Gameover you got {score} questions correct."
-    question = [message, "L", "L", "L", "L", 5]
+    question = [message, "THE END", "THE END", "THE END", "THE END", 5]
     time_left = 0
     is_gameover = True
 
@@ -98,7 +98,7 @@ def skip_question():
     global time_left, question
     if questions and not is_gameover:
         question = read_next_question()
-        time_left = 10
+        time_left = 100
     else:
         gameover()
 
@@ -114,5 +114,5 @@ def update():
 
 read_question_file()
 question  = read_next_question()
-clock.schedule_interval(update_time_left,1)
+clock.schedule_interval(update_time_left,0.05)
 pgzrun.go()
