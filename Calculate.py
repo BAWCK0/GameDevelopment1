d4e5c0726cd8b982bad97f7ded61ae3cb86bb88d
BAWCK0 <sorton9.208@gmail.com>
2025-09-04 18:52:56 +0100

    Add files via upload

--- Calculate.py
+++ Calculate.py
@@ -55,8 +55,35 @@ secondtitle_label = tk.Label(root, text="Transitioning Lengths")
 secondtitle_label.place(x=700//2, y=300)
 
 def translate():
-    try:
-        if firstunit_entry.get() == "":
+
+        if firstunit_entry.get() == "mm":
+            if secondunit_entry.get() == "mm":
+                result = int(length_entry.get())
+                
+            elif secondunit_entry.get() == "cm":
+                result = int(length_entry.get())*10
+                
+            elif secondunit_entry.get() == "dm":
+                result = int(length_entry.get())*100
+                
+            elif secondunit_entry.get() == "m":
+                result = int(length_entry.get())*1000
+                
+            elif secondunit_entry.get() == "km":
+                result = int(length_entry.get())*1000000
+                
+                
+        
+                
+        answer_box.config(text=result)
+
+        print("no")
+            
+    
+            
+        
+            
+        
             
 
 length_entry = tk.Entry(root, width=50)
@@ -69,6 +96,7 @@ secondunit_entry = tk.Entry(root, width = 50)
 secondunit_entry.place(x=350, y=380)
 
 button = tk.Button(root, text="OK", command=translate)
+button.place(x=350, y=500)
 
 answer_box = tk.Label(root, text="MTEE")
 answer_box.place(x=700//2.5, y=480)
