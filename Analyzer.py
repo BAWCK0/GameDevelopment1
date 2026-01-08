054fd321c6c5252121c7b2a69eb7161a683da40c
BAWCK0 <sorton9.208@gmail.com>
2026-01-08 18:07:32 +0000

    Update Analyzer.py

--- Analyzer.py
+++ Analyzer.py
@@ -2,13 +2,23 @@ from tkinter import *
 
 ROOT = Tk()
 
-NOUNS = ["dog", "dogs", "cat", "cats"]
-VERBS = ["running", "eating", "sleeping"]
+NOUNS = ["dog", "dogs", "cat", "cats", "hamster"]
+VERBS = ["running", "ran","run", "eating", "sleeping", "ate", "come", "hamsterified"]
 
 def find_stuff():
     text = input.get()
+    found_nouns = []
+    found_verbs = []
     for noun in NOUNS:
-        if noun in 
+        if noun in text:
+            found_nouns.append(noun)
+        
+    for verb in VERBS:
+        if verb in text:
+            found_verbs.append(verb)
+            
+    output.config(text=f"{found_verbs[0]} is a verb and {found_nouns[0]} is a noun.")
+            
 
 title = Label(text="Analyzer", font=("Arial", 32, "bold"))
 title.grid(column=0, row=0)
